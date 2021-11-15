20666af808be45dee1cf231294c5ea9c0ca2acfc
katrushki <katerina.zafeiri@code.berlin>
2021-11-15 22:14:03 +0100

    MeetPref and MeetFor Screen boilerplate setup

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import WelcomeOBS from './src/screens/onboarding/I_Welcome';
-import AllSetOBS from './src/screens/onboarding/VI_AllSet';
+import AllSetOBS from './src/screens/onboarding/VII_AllSet';
 import BasicInfo from './src/screens/onboarding/II_BasicInfo';
 import BasicInfoOBS from './src/screens/onboarding/II_BasicInfo';
 
---
+++ src/screens/onboarding/V_MeetPref.js
@@ -0,0 +1,16 @@
+// MEETING PREFERENCE SCREEN - in person, Zoom, or prefer not to meet
+
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const MeetPrefOBS = () => {
+    return (
+        <View>
+            <Text>MeetPref</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({});
+
+export default MeetPrefOBS;
